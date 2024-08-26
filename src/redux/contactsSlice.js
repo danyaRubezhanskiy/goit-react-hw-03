e1af5612552bed21fb0293498546e6f7bec894b0
b1mbula <danja.rubeghanskij@gmail.com>
2024-08-26 18:50:52 +0200

    hw

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,9 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const contactSlice = createSlice({
+const initialContacts = [
+  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+];
+
+const contactsSlice = createSlice({
   name: "contacts",
   initialState: {
-    items: [],
+    items: initialContacts,
   },
   reducers: {
     addContact: (state, action) => {
@@ -17,8 +24,6 @@ const contactSlice = createSlice({
   },
 });
 
-export const { addContact, deleteContact } = contactSlice.actions;
-
+export const { addContact, deleteContact } = contactsSlice.actions;
 export const selectContacts = (state) => state.contacts.items;
-
-export default contactSlice.reducer;
+export default contactsSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,16 @@ import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import contactsReducer from "./contactsSlice";
 import filtersReducer from "./filtersSlice";
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
+// Конфигурация persist
 const persistConfig = {
   key: "contacts",
   storage,
@@ -17,7 +26,16 @@ const store = configureStore({
     contacts: persistedContactsReducer,
     filters: filtersReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-export const persistor = persistStore(store);
+const persistor = persistStore(store);
+
+export { store, persistor };
+
 export default store;
